6c88c04a509abe71785a7b511b66e3b4e0bda22c
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-15 13:28:17 -0300

    chore(gh-action): fix git push not working

--- publish.sh
+++ publish.sh
@@ -13,9 +13,15 @@ fi
 
 git clone --depth=1 "$MAVEN_REPO_GIT_REPO" "$MAVEN_REPO_DIR"
 
-echo "Setting git config..."
-git config --local user.name 'github-actions[bot]'
-git config --local user.email 'github-actions[bot]@users.noreply.github.com'
+if [[ -n "${GH_TOKEN}" ]]; then
+    echo "GH_TOKEN is set; setting appropriate git config for workflow"
+    git config --local user.name 'github-actions[bot]'
+    git config --local user.email 'github-actions[bot]@users.noreply.github.com'
+    # https://stackoverflow.com/a/57229018
+    git config --local url."https://api:$GH_TOKEN@github.com/".insteadOf "https://github.com/"
+    git config --local url."https://ssh:$GH_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+    git config --local url."https://git:$GH_TOKEN@github.com/".insteadOf "git@github.com:"
+fi
 
 echo "Building and publishing..."
 cd "$SCRIPT_DIR"
