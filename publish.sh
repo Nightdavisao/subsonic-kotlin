9bf86976a4d5c7e7a1504141e5d3c745a96d4f73
Nightdavisao <nightdavisao27@protonmail.com>
2026-09-15 13:45:42 -0300

    chore(gh-action): i hate this shit

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+PROJECT_GROUP=$(sed -n 's/.*group = "\(.*\)".*/\1/p' build.gradle.kts)
+PROJECT_VERSION=$(sed -n 's/.*version = "\(.*\)".*/\1/p' build.gradle.kts)
+
 MODULES=(subsonic-client subsonic-api)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 MAVEN_REPO_GIT_REPO="https://github.com/Nightdavisao/maven-repo.git"
@@ -12,16 +15,6 @@ if [[ -n "$MAVEN_REPO_DIR" ]]; then
 fi
 
 git clone --depth=1 "$MAVEN_REPO_GIT_REPO" "$MAVEN_REPO_DIR"
-
-if [[ -n "${GH_TOKEN}" ]]; then
-    echo "GH_TOKEN is set; setting appropriate git config for workflow"
-    git config --local user.name 'github-actions[bot]'
-    git config --local user.email 'github-actions[bot]@users.noreply.github.com'
-    # https://stackoverflow.com/a/57229018
-    git config --local url."https://api:$GH_TOKEN@github.com/".insteadOf "https://github.com/"
-    git config --local url."https://ssh:$GH_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
-    git config --local url."https://git:$GH_TOKEN@github.com/".insteadOf "git@github.com:"
-fi
 
 echo "Building and publishing..."
 cd "$SCRIPT_DIR"
@@ -35,10 +28,22 @@ done
 
 echo "Pushing to Git..."
 cd "$MAVEN_REPO_DIR"
+
+if [[ -n "${GH_TOKEN}" ]]; then
+    # does --local even work here? i don't fucking know
+    echo "GH_TOKEN is set; setting appropriate git config for workflow"
+    cmd_prefix="git config --local"
+
+    $cmd_prefix user.name 'github-actions[bot]'
+    $cmd_prefix user.email 'github-actions[bot]@users.noreply.github.com'
+    # https://stackoverflow.com/a/57229018
+    $cmd_prefix url."https://api:$GH_TOKEN@github.com/".insteadOf "https://github.com/"
+    $cmd_prefix url."https://ssh:$GH_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+    $cmd_prefix url."https://git:$GH_TOKEN@github.com/".insteadOf "git@github.com:"
+fi
+
 git add -A
-git commit -m "publish: $MODULE_NAME $(grep '^version' \
-    "$SCRIPT_DIR/$MODULE_NAME/build.gradle.kts" | head -1 | \
-    sed 's/.*"\(.*\)".*/\1/')"
-git push
+git commit -m "publish: $PROJECT_GROUP ($PROJECT_VERSION)"
+git push --force-with-lease
 
 echo "Done!"
